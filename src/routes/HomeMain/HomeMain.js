6bc6f59a1c413c93c5206b783bb3c71898858332
Phoebe Law <phoebebasilio@gmail.com>
2019-10-23 21:51:54 -0700

    adds 'max widths' for pages and nav bars

--- src/routes/HomeMain/HomeMain.js
+++ src/routes/HomeMain/HomeMain.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function HomeMain () {
   return (
-    <>
+    <div className='HomeMain__div mainContainer'>
       <header className="App__header">
         <h1>FridgiDare</h1>
         <h2>Challenging Food Waste, One Grocery Trip At A Time</h2>
@@ -14,6 +14,6 @@ export default function HomeMain () {
       <h3>Briefly explain problem/how app is a solution</h3>
       <p>(forget what's in fridge when shopping (buy extra) and forget in general (goes bad)</p>
 
-    </>
+    </div>
   )
 }
